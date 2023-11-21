e8e94cc98467ee4159079ee5dcd49ed05c8cd564
Vitor Aun Kassab <vitor.kassab@usp.br>
2023-11-21 00:38:05 -0300

    Adicionar classe Post com Create e detail

--- albuns/models.py
+++ albuns/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField(default="")
+    post_date = models.DateTimeField(default=timezone.now)
+    
+    band = models.CharField(max_length=30)
+    release_year = models.IntegerField()
+    poster_url = models.URLField(max_length=200, null=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.release_year})'
+    
+
+
+
--- albuns/views.py
+++ albuns/views.py
@@ -6,36 +6,45 @@ from .temp_data import album_data
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from .models import Post
+
+from django.shortcuts import render, get_object_or_404
 
 
-
-def detail_album(request, album_id):
-    context = {'album': album_data[album_id - 1]}
-    return render(request, 'albuns/detail.html', context)
 
 def list_albuns(request):
-    context = {"album_list": album_data}
+    album_list = Post.objects.all()
+    context = {'album_list': album_list}
     return render(request, 'albuns/index.html', context)
+
+def detail_album(request, album_id):
+    album = get_object_or_404(Post, pk=album_id)
+    context = {'album': album}
+    return render(request, 'albuns/detail.html', context)
+
+
 
 def search_albuns(request):
     context = {}
     if request.GET.get('query', False):
-        context = {
-            "album_list": [
-                m for m in album_data
-                if request.GET['query'].lower() in m['name'].lower()
-            ]
-        }
+        search_term = request.GET['query'].lower()
+        album_list = Post.objects.filter(title__icontains=search_term)
+        context = {"album_list": album_list}
     return render(request, 'albuns/search.html', context)
 
 def create_album(request):
     if request.method == 'POST':
-        album_data.append({
-            'name': request.POST['name'],
-            'release_year': request.POST['release_year'],
-            'poster_url': request.POST['poster_url']
-        })
+        album_title = request.POST['title']
+        album_band = request.POST['band']
+        album_release_year = request.POST['release_year']
+        album_content = request.POST['content']
+        
+        album_poster_url = request.POST['poster_url']
+        album = Post(title=album_title,
+                      release_year=album_release_year,
+                      poster_url=album_poster_url, band = album_band, content = album_content)
+        album.save()
         return HttpResponseRedirect(
-            reverse('albuns:detail', args=(len(album_data), )))
+            reverse('albuns:detail', args=(album.id, )))
     else:
         return render(request, 'albuns/create.html', {})
