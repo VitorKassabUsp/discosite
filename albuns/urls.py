a026e08f601c73ee3a76a1bb62b03b66f2cee71e
Vitor Aun Kassab <vitor.kassab@usp.br>
2023-11-21 03:36:25 -0300

    Tamanho do logo arrumado e CRUD 3 funcionando parcialmente

--- albuns/urls.py
+++ albuns/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path
 
 from . import views
+from .views import PostDetailView, PostListView,  PostSearchView, PostCreateView, PostUpdateView,PostDeleteView
+
+
 
 app_name = 'albuns'
 urlpatterns = [
-    path('', views.list_albuns, name='index'),
-    path('search/', views.search_albuns, name='search'),
-    path('create/', views.create_album, name='create'),
-    path('<int:album_id>/', views.detail_album, name='detail'),
-    path('update/<int:album_id>/', views.update_album, name='update'),
-    path('delete/<int:album_id>/', views.delete_album, name='delete'),
+    #path('', views.list_albuns, name='index'),
+    path('albuns/', PostListView.as_view(), name='index'),
+
+    #path('search/', views.search_albuns, name='search'),
+    path('albuns/search/', PostSearchView.as_view(), name='search'),
+
+    #path('create/', views.create_album, name='create'),
+    path('albuns/create/', PostCreateView.as_view(), name='create'),
+
+    #path('<int:album_id>/', views.detail_album, name='detail'),
+    path('albuns/<int:album_id>/', PostDetailView.as_view(), name='detail'),
+    
+    #path('update/<int:album_id>/', views.update_album, name='update'),
+    path('albuns/<int:pk>/update/', PostUpdateView.as_view(), name='update'),
+
+    #path('delete/<int:album_id>/', views.delete_album, name='delete'),
+    path('albuns/<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
 ]
--- albuns/views.py
+++ albuns/views.py
@@ -5,35 +5,56 @@ from .temp_data import album_data
 
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-
-
-def list_albuns(request):
+'''def list_albuns(request):
     album_list = Post.objects.all()
     context = {'album_list': album_list}
     return render(request, 'albuns/index.html', context)
+'''
+class PostListView(ListView):
+    model = Post
+    template_name = 'albuns/index.html'
+    context_object_name = 'album_list'
 
-def detail_album(request, album_id):
+'''def detail_album(request, album_id):
     album = get_object_or_404(Post, pk=album_id)
     context = {'album': album}
     return render(request, 'albuns/detail.html', context)
+'''
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'albuns/detail.html'
+    context_object_name = 'album'
+    pk_url_kwarg = 'album_id'
 
-
-
-def search_albuns(request):
+'''def search_albuns(request):
     context = {}
     if request.GET.get('query', False):
         search_term = request.GET['query'].lower()
         album_list = Post.objects.filter(title__icontains=search_term)
         context = {"album_list": album_list}
     return render(request, 'albuns/search.html', context)
+'''
+class PostSearchView(ListView):
+    model = Post
+    template_name = 'albuns/search.html'
+    context_object_name = 'album_list'
 
-def create_album(request):
+    def get_queryset(self):
+        query = self.request.GET.get('query', '')
+        if query:
+            return Post.objects.filter(title__icontains=query.lower())
+        else:
+            return Post.objects.none()
+        
+'''def create_album(request):
     if request.method == 'POST':
         album_title = request.POST['title']
         album_band = request.POST['band']
@@ -51,7 +72,14 @@ def create_album(request):
         form = PostForm()
         context = {'form': form}
         return render(request, 'albuns/create.html', context)
-    
+ ''' 
+class PostCreateView(CreateView): #cria, mas nao manda pro detalhes após criar
+    model = Post
+    form_class = PostForm
+    template_name = 'albuns/create.html'
+    success_url = reverse_lazy('albuns:create')
+
+'''
 def update_album(request, album_id):
     album = get_object_or_404(Post, pk=album_id)
 
@@ -67,8 +95,17 @@ def update_album(request, album_id):
 
     context = {'album': album}
     return render(request, 'albuns/update.html', context)
+'''
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'albuns/update.html'
+    context_object_name = 'album'
 
+    def get_success_url(self):
+        return reverse_lazy('albuns:detail', args=(self.object.id,))
 
+'''
 def delete_album(request, album_id):
     album = get_object_or_404(Post, pk=album_id)
 
@@ -78,3 +115,10 @@ def delete_album(request, album_id):
 
     context = {'album': album}
     return render(request, 'albuns/delete.html', context)
+    '''
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'albuns/delete.html'
+    context_object_name = 'album'
+    success_url = reverse_lazy('albuns:index')
